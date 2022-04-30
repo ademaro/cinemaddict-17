9db9b25a9a800208ada9d656d091fd2005058455
Anton Popov <ademaro@ya.ru>
2022-04-30 13:54:39 +0530

    Добавлен рендер шапки и инициализация презентера

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import ProfileView from './view/profile-view.js';
+import FilmsPresenter from './presenter/films-presenter.js';
+import { render } from './render.js';
+
+const headerElement = document.querySelector('.header');
+render(new ProfileView(), headerElement);
+
+const mainElement = document.querySelector('.main');
+const filmsPresenter = new FilmsPresenter();
+filmsPresenter.init(mainElement);
