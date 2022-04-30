dbb015c5f6294890df1ca40d8d19e896074c2d3b
Anton Popov <ademaro@ya.ru>
2022-04-30 13:53:57 +0530

    Добавлен презентер списка фильмов

---
+++ src/presenter/films-presenter.js
@@ -0,0 +1,31 @@
+import NavigationView from '../view/navigation-view.js';
+import SortView from '../view/sort-view.js';
+import FilmsView from '../view/films-view.js';
+import FilmsListView from '../view/films-list-view.js';
+import FilmCardView from '../view/film-card-view.js';
+import ShowMoreView from '../view/show-more-view.js';
+import PopupView from '../view/popup-view.js';
+import { render } from '../render.js';
+
+const FILMS_COUNT = 5;
+
+export default class FilmsPresenter {
+  init = (mainElement) => {
+    this.mainElement = mainElement;
+
+    render(new NavigationView(), this.mainElement);
+    render(new SortView(), this.mainElement);
+    render(new FilmsView(), this.mainElement);
+
+    const filmListView = new FilmsListView();
+    render(filmListView, this.mainElement);
+
+    for (let i = 0; i < FILMS_COUNT; i++) {
+      render(new FilmCardView(), filmListView.getElement().querySelector('.films-list__container'));
+    }
+
+    render(new ShowMoreView(), filmListView.getElement());
+
+    render(new PopupView(), this.mainElement);
+  };
+}
